651208524ab9d88fc9d2df33335093c13f75adcc
DoctorXu <boshi.xu@hotmail.com>
2017-11-08 00:37:25 +0800

    XiaoMi-PC Second Commit

--- MyModule/src/main/java/com/example/myapplication/MainActivity.java
+++ MyModule/src/main/java/com/example/myapplication/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.d("MainActivity", "MainActivity1");
+        Log.d("MainActivity", "MainActivity2");
     }
 }
